1d1e16a9ca27f40c498ff335477489cd798e6b54
kervens <kervensford5@gmail.com>
2019-06-05 12:31:56 -0400

    intro

---
+++ introduccion_javascript_101/0_script.js
@@ -0,0 +1,33 @@
+alert("Hola a todos !");
+
+// pa pale kaka li pa bon pou ou//
+/*Welcome to the world of sentence of death*/ 
+
+// Pedir al usuario que digite su nombre  //
+var nombre = prompt("Digite su nombre ");
+var edad = prompt("Digite tu edad ");
+var ciudad = prompt("Digite tu ciudad ");
+var tarjeta_Credito = prompt("Digite tu tarjeta de Credito ");
+
+console.log(nombre);
+console.log(edad);
+console.log(ciudad);
+console.log(tarjeta_Credito);
+
+document.write(nombre);
+document.write(edad);
+document.write(ciudad);
+document.write(tarjeta_Credito);
+
+console.log(document.body);
+console.log(document.head);
+console.log(document.height);
+console.log(document.URL);
+
+var pais;
+console.log(pais);
+
+var pais ="japan";
+console.log(pais);
+
+document.title = "Nuevo titulo";
---
+++ introduccion_javascript_101/ejecio.js
@@ -0,0 +1,20 @@
+var nombre = prompt("Digite su nombre ");
+var edad = prompt("Digite tu edad ");
+var ciudad  = prompt("Digite tu ciudad ");
+var barrio = prompt("Digite tu barrio ");
+var direccion  = prompt("Digite tu direccion ");
+console.log(nombre);
+console.log(edad);
+console.log(ciudad);
+console.log(barrio);
+console.log(direccion);
+
+document.write(`<h1> ${nombre}</h1>`);
+document.write(`<p> ${edad}</p>`);
+document.write(`<p> ${ciudad}</p>`);
+document.write(`<p> ${barrio}</p>`);
+document.write(`<p> ${direccion}</p>`);
+
+
+
+/*hacer que el usuario digite su ciudad,barrio y direccion ,imprimirlos en un div*/ 
