3892f949059030f9a26b1ef8fba5c3656c1c60e2
kervens <kervensford5@gmail.com>
2019-06-12 12:09:08 -0400

    fordProducts

---
+++ proyecto/mi_pagina.js
@@ -0,0 +1,20 @@
+alert("Welcome This page give us Technology knowledge and sell product !");
+(alert);
+
+alert("To have acces to this websites we will need some infos in your hand!");
+(alert);
+
+var FirstName = prompt ("Put your First Name");
+console.log(FirstName);
+var LastName = prompt ("Put your Last Name");
+console.log(LastName);
+var Age = prompt ("Put your Age");
+console.log(Age);
+
+var Like = prompt("Do you like sex");
+console.log(Like);
+
+document.write(`<h1> ${FirstName}</h1>`);
+document.write(`<h2><i> ${LastName}</i></h2>`);
+document.write(`<h1> ${Age}</h1>`);
+document.write(`<h1> ${Like}</h1>`);
